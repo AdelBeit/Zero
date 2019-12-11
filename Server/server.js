2fc12635d22b4aca530cac37b6440ea41f85ffbe
adelbeit <adelbeit@gmail.com>
2019-12-11 00:14:19 -0800

    everything works need to find a meme generator api

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,40 @@
 const https = require('http');
 const port = process.env.PORT || 3000;
+const bot = require('./../Bot/bot');
+const fs = require('fs');
 
+const server = https.createServer();
 
-const server = https.createServer( (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end("Good bye\n");
+server.on('request', (request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    let url = request.url, method = request.method;
+
+    // store the request data for later processing
+    request.chunks = [];
+    request.on('data', chunk => {
+        request.chunks.push(chunk);
+    });
+
+    request.on('end', () => {
+        if(method === 'POST'){
+            bot.response = response;
+            bot.request = request;
+            bot.respond();
+            response.end();
+        }
+    });
+
+    if(method === 'GET'){
+        response.write('<h1>Welcome to the bot homepage</h1>');
+        response.end();
+    }
+
+    response.on('error', error => {
+        response.writeHead(200);
+        response.end(error);
+    });
 });
 
 server.listen(port, () => {
-    console.log("Server is running at " + port)
+    console.log('Server listening on localhost:' + port);
 });

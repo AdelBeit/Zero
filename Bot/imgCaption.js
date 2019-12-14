3e906a035573a1fe2bf1652f033cef93b4cbf9b5
adelbeit <adelbeit@gmail.com>
2019-12-14 04:13:40 -0800

    caption function works, gotta tweak the text placement

--- Bot/imgCaption.js
+++ Bot/imgCaption.js
@@ -11,7 +11,7 @@ let exportDir = './Assets/export/';
  * Caption an image and return the path
  */
 function caption(rawPath, caption){
-  caption.text = caption,
+  caption = {'text':caption};
   caption.x = 10,
   caption.y = 20,
   caption.maxWidth = 200,
@@ -20,23 +20,24 @@ function caption(rawPath, caption){
   jimp.read(rawPath)
   .then(image => {image.clone().write(activeDir+name)})
 
-  .then(() => {jimp.read(activeDir+name)})
+  .then(() => {jimp.read(activeDir+name)
 
-  .then(image => {jimp.loadFont(jimp.FONT_SANS_32_WHITE).then(font => {[image,font]})})
+    .then(image => {
 
-  .then(data => {
-    let image = data[0], font = data[1];
+      jimp.loadFont(jimp.FONT_SANS_10_BLACK).then(font => {    
+        return image.print(font, caption.x, caption.y, {
+          text: caption.text,
+          alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+          alignmentY: jimp.VERTICAL_ALIGN_BOTTOM
+        }, caption.maxWidth, caption.maxHeight);
+      })
+    
+      .then(image => {image.quality(100).write(exportDir+name)})
+      
+      .then(image => {console.log('exported image: ' + exportDir+name)})
 
-    return image.print(font, caption.x, caption.y, {
-      text: caption.text,
-      alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-      alignmentY: jimp.VERTICAL_ALIGN_BOTTOM
-    }, caption.maxWidth, caption.maxHeight);
+    })
   })
-
-  .then(image => {image.quality(100).write(exportDir+name)})
-  
-  .then(image => {console.log('exported image: ' + exportDir+name)})
 
   .catch(error => {console.error(error)});
 }
